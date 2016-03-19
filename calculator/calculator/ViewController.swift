faf96a41603a13ce1848286da3caa3b74cff8bca
yujiikebata <yujiikebata@gmail.com>
2016-03-19 10:42:07 +0900

    create calculator project

---
+++ calculator/calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by 池端　優二 on 3/19/16.
+//  Copyright © 2016 yujiikebata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
