77aa6ed4e749f580b0a83738597bda8153686f37
yujiikebata <yujiikebata@gmail.com>
2016-03-19 10:52:52 +0900

    write subtraction on label

--- calculator/calculator/ViewController.swift
+++ calculator/calculator/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let value: Int = 10 - 1
+        label.text = "10 - 1 = \(value)"
     }
 
     override func didReceiveMemoryWarning() {
