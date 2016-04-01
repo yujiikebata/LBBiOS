d3d714c64cd45c89d25c7d692284da0096ad78e9
yujiikebata <yujiikebata@gmail.com>
2016-04-02 08:30:56 +0900

    create percent view controller

---
+++ MyCalc/MyCalc/PercentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PercentViewController.swift
+//  MyCalc
+//
+//  Created by 池端　優二 on 4/2/16.
+//  Copyright © 2016 yujiikebata. All rights reserved.
+//
+
+import UIKit
+
+class PercentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
