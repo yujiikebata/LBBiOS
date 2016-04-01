b270dd822328e100052da0ed0b5d6f3ea4c6f1b4
yujiikebata <yujiikebata@gmail.com>
2016-04-01 20:52:24 +0900

    start to develop calculator that get discount prices

---
+++ MyCalc/MyCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCalc
+//
+//  Created by 池端　優二 on 4/1/16.
+//  Copyright © 2016 yujiikebata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
