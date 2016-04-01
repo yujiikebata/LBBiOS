ea536fc2a1f62db4795cb3aedcd7ace7455841b7
yujiikebata <yujiikebata@gmail.com>
2016-04-02 08:46:12 +0900

    implement tap number and clear method

--- MyCalc/MyCalc/PercentViewController.swift
+++ MyCalc/MyCalc/PercentViewController.swift
@@ -2,14 +2,17 @@
 //  PercentViewController.swift
 //  MyCalc
 //
-//  Created by 池端　優二 on 4/2/16.
+//  Created by yujiikebata on 4/2/16.
 //  Copyright © 2016 yujiikebata. All rights reserved.
 //
 
 import UIKit
 
 class PercentViewController: UIViewController {
-
+    
+    var price: Int = 0
+    @IBOutlet weak var percentField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +24,80 @@ class PercentViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func tap1Button(sender: AnyObject) {
+        let value = percentField.text! + "1"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
 
+    @IBAction func tap2Button(sender: AnyObject) {
+        let value = percentField.text! + "2"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap3Button(sender: AnyObject) {
+        let value = percentField.text! + "3"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
+
+    @IBAction func tap4Button(sender: AnyObject) {
+        let value = percentField.text! + "4"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap5Button(sender: AnyObject) {
+        let value = percentField.text! + "5"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap6Button(sender: AnyObject) {
+        let value = percentField.text! + "6"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap7Button(sender: AnyObject) {
+        let value = percentField.text! + "7"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap8Button(sender: AnyObject) {
+        let value = percentField.text! + "8"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap9Button(sender: AnyObject) {
+        let value = percentField.text! + "9"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap0Button(sender: AnyObject) {
+        let value = percentField.text! + "0"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tapClearButton(sender: AnyObject) {
+        percentField.text = "0"
+    }
+    
     /*
     // MARK: - Navigation
 
