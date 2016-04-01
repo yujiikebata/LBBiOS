0ef76138665476085aaf5451fee220abbaca9671
yujiikebata <yujiikebata@gmail.com>
2016-04-01 21:41:24 +0900

    implement tap1Button method

--- MyCalc/MyCalc/ViewController.swift
+++ MyCalc/MyCalc/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var priceField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tap1Button(sender: AnyObject) {
+        let value = priceField.text! + "1"
+        if let price = Int(value) {
+            priceField.text = "\(price)"
+        }
+    }
 }
 
