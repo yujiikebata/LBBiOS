21970d2d62da3bbddb1e6e18a1836d28626f79b4
yujiikebata <yujiikebata@gmail.com>
2016-04-02 20:14:32 +0900

    store and retrieve input text

--- SampleDataStorage/SampleDataStorage/ViewController.swift
+++ SampleDataStorage/SampleDataStorage/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        // retrieve stored text
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        if let value = userDefaults.stringForKey("text") {
+            // set it
+            textField.text = value
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tapActionButton(sender: AnyObject) {
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        userDefaults.setObject(textField.text, forKey: "text")
+        userDefaults.synchronize()
+    }
 }
 
