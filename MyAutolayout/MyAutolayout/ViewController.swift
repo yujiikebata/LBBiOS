c58e7f9878cc8b7f20ddfa2b2ce346b44bf93691
yujiikebata <yujiikebata@gmail.com>
2016-04-02 18:50:49 +0900

    play with auto layout

---
+++ MyAutolayout/MyAutolayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyAutolayout
+//
+//  Created by 池端　優二 on 4/2/16.
+//  Copyright © 2016 yujiikebata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
