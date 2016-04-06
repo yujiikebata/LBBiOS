85a7e928fc465f5192fcf4945ae9be1718e42a45
yujiikebata <yujiikebata@gmail.com>
2016-04-06 21:29:18 +0900

    append todo list

--- ToDoApp/ToDoApp/ViewController.swift
+++ ToDoApp/ToDoApp/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var todoList = [String]()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tapAddButton(sender: AnyObject) {
+        let alertController = UIAlertController(title: "TODO追加",
+                                                message: "TODOを入力してください",
+                                                preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addTextFieldWithConfigurationHandler(nil)
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default) {
+            (action: UIAlertAction) -> Void in
+            if let textField = alertController.textFields?.first {
+                self.todoList.insert(textField.text!, atIndex: 0)
+                
+                self.tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: 0, inSection: 0)], withRowAnimation: UITableViewRowAnimation.Right)
+            }
+        }
+        
+        alertController.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "CANCEL", style: UIAlertActionStyle.Cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
+    }
 }
 
