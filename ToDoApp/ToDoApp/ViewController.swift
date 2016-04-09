48a4dbd30e1cb201c0397e876351e2a7d48fabc8
yujiikebata <yujiikebata@gmail.com>
2016-04-09 18:41:58 +0900

    load user data in view did load

--- ToDoApp/ToDoApp/ViewController.swift
+++ ToDoApp/ToDoApp/ViewController.swift
@@ -16,7 +16,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // load user data
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        if let storedTodoList = userDefaults.arrayForKey("todoList") as? [String] {
+            todoList.appendContentsOf(storedTodoList)
+        }
     }
 
     override func didReceiveMemoryWarning() {
