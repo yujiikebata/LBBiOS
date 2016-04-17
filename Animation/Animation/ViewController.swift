1545bb533edc4f90fae3a691a0b5fd318a0ae7ba
yujiikebata <yujiikebata@gmail.com>
2016-04-17 22:25:28 +0900

    set up playground for Animation

---
+++ Animation/Animation/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by yujiikebata on 4/17/16.
+//  Copyright © 2016 yujiikebata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var targetView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func tapStartButton(sender: AnyObject) {
+    }
+}
+
